97aafe7b8ba8a7e63f68f890b5486904c3c98468
wulman16 <will.ulman@gmail.com>
2019-04-05 16:14:59 -0400

    done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const reviewStyle = {
+  border: '5px solid black',
+  margin: '5px',
+  padding: '5px'
+}
+
+const MovieReviews = (props) => ( 
+    <div className="review-list">
+      { props.reviews.map(review => {
+        return(
+        <div className="review" style={reviewStyle}>
+          <h3>{review.headline}</h3>
+          <h4>{review.byline}</h4>
+          <h4>{review.publication_date}</h4>
+          <p>{review.summary_short}</p>
+        </div>
+        )
+      } )}
+    </div>
+)
+
+export default MovieReviews
